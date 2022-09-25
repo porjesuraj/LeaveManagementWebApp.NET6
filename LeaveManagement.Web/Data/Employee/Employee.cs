b8dfaa4543b0711ebb536651db8065026a1d4c8e
porjesuraj <porjesuraj5@gmail.com>
2022-09-25 19:51:05 +0530

    Using Code First Approach Added Database Tables and Migration , Applied DRY principle  for creating Table Entities

---
+++ LeaveManagement.Web/Data/Employee/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace LeaveManagement.Web.Data.Employee
+{
+    public class Employee: Microsoft.AspNetCore.Identity.IdentityUser
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? TaxId { get; set; }
+
+        public DateTime JoiningDate { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ LeaveManagement.Web/Data/Migrations/20220925071038_UpdateIndentiyUserWithEmployee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveManagement.Web.Data.Migrations
+{
+    public partial class UpdateIndentiyUserWithEmployee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "JoiningDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TaxId",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "JoiningDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TaxId",
+                table: "AspNetUsers");
+        }
+    }
+}
